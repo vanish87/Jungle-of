460aea9ac5cf409cf1089b621fad8ca27249b931
Franziska Zeiner <franziska.zeiner@gmail.com>
2013-04-25 17:41:54 -0400

    changed backgroundgradientcolor low to 105-119-120

--- src/StaticScene.cpp
+++ src/StaticScene.cpp
@@ -105,7 +105,7 @@ namespace Jungle
         //set background here
         ofBackground(14, 28, 54);
         
-        ofBackgroundGradient(ofColor(203,234,235),ofColor(14,28,54), OF_GRADIENT_LINEAR);
+        ofBackgroundGradient(ofColor(105,119,120),ofColor(14,28,54), OF_GRADIENT_LINEAR);
 		glEnable(GL_DEPTH_TEST);
 
 		ofPushMatrix();				
